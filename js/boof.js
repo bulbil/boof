bfceef10d4ce6fac1002a3eb8ae31e20de2bf601
Nabil Kashyap <n@nabilk.com>
2015-02-13 12:14:41 -0500

    fixing crystal mesh issue

--- js/boof.js
+++ js/boof.js
@@ -73,9 +73,12 @@ function update() {
     requestAnimationFrame( update );
     var timer = Date.now() * 0.001;
  
-    crystalMesh.rotation.y -= 0.002;
-    crystalMesh.rotation.y = ( Math.PI / 2 ) - timer * 0.5;
-    crystalMesh.rotation.z = timer * 0.1;
+    if(crystelMesh){
+        crystalMesh.rotation.y -= 0.002;
+        crystalMesh.rotation.y = ( Math.PI / 2 ) - timer * 0.5;
+        crystalMesh.rotation.z = timer * 0.1;
+    }
+
     controls.update();
     groundMirror.render();
     renderer.render(scene, camera);
@@ -112,15 +115,15 @@ function draw(){
     
     function loadCrystal(geometry, material){
         
-        d= new THREE.Mesh(geometry, material);
-        material.scale.x = 10;
-        material.scale.y = 10;
-        material.scale.z = 10;
-        material.position.x = 0;
-        material.position.y = 8;
-        material.position.z = 15;
-        material.updateMatrix();
-        scene.add(material);
+        crystalMesh = new THREE.Mesh(geometry, material);
+        crystalMesh.scale.x = 10;
+        crystalMesh.scale.y = 10;
+        crystalMesh.scale.z = 10;
+        crystalMesh.position.x = 0;
+        crystalMesh.position.y = 8;
+        crystalMesh.position.z = 15;
+        crystalMesh.updateMatrix();
+        scene.add(crystalMesh);
     }
 
     loader = new THREE.JSONLoader();
